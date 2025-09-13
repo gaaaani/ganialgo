975101e406e4a9ed5c3cc86492945a91adfdf706
gaaaani (서가은) <155754445+gaaaani@users.noreply.github.com>
2025-09-13 20:39:04 +0900

    [level 0] Title: 모음 제거, Time: 0.20 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,13 +1,14 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        System.out.println(my_string);
-        for(int i = 0; i<my_string.length(); i++){
-            char ch = my_string.charAt(i);
-                if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
-                answer += ch;
-            }               
-        }
+        String answer = my_string.replaceAll("a|e|i|o|u", "");
+        // System.out.println(my_string);
+        // for(int i = 0; i<my_string.length(); i++){
+        //     char ch = my_string.charAt(i);
+        //         if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u') {
+        //         answer += ch;
+        //     }               
+        // }
+        
         return answer;
     }
 }
