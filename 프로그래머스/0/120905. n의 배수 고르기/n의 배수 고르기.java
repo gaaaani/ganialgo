96716aedab1130a61ca7251b1234c44303a7a5d6
gaaaani (서가은) <155754445+gaaaani@users.noreply.github.com>
2025-10-21 21:56:27 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int[] numlist) {
+        // int[] answer = {};
+        List<Integer> arr = new ArrayList<>();
+        for(int i = 0; i<numlist.length; i++){
+            if(numlist[i] % n == 0){
+                arr.add(numlist[i]);
+            }
+        }
+        int[] answer = new int[arr.size()];
+        for(int i =0; i<arr.size(); i++){
+            answer[i] = arr.get(i);
+        }
+        return answer;
+    }
+}
