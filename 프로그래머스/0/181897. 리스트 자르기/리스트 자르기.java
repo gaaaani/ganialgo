bd5eb96e67326be74adec73591da0b77c206bd1b
gaaaani (서가은) <155754445+gaaaani@users.noreply.github.com>
2025-11-13 22:05:05 +0900

    [level 0] Title: 리스트 자르기, Time: 0.05 ms, Memory: 90.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> newint = new ArrayList<>();
+        int a = slicer[0];
+        int b = slicer[1];
+        int c = slicer[2];
+        if(n==1){
+            for(int i = 0; i<=b; i++){
+                newint.add(num_list[i]);
+            }
+        }else if(n==2){
+            for(int i = a; i<num_list.length; i++){
+                newint.add(num_list[i]);
+            }
+        }else if(n==2){
+            for(int i = a; i<num_list.length; i++){
+                newint.add(num_list[i]);
+            }
+        }else if(n==3){
+            for(int i = a; i<=b; i++){
+                newint.add(num_list[i]);
+            }
+        }else if(n==4){
+            for(int i = a; i<=b; i+=c){
+                newint.add(num_list[i]);
+            }
+        }
+        
+        int[] answer = new int[newint.size()];
+        for(int i = 0; i<answer.length; i++){
+            answer[i] = newint.get(i);
+        }
+        return answer;
+    }
+}
