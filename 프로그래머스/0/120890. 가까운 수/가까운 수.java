a2bd36757306975d7c156b769a50466ca9a530bb
gaaaani (서가은) <155754445+gaaaani@users.noreply.github.com>
2025-11-14 17:23:43 +0900

    [level 0] Title: 가까운 수, Time: 0.54 ms, Memory: 86.3 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.java
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.java
@@ -1,24 +1,15 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] array, int n) {
-        int m = Math.abs(array[0] - n);
-        int answer = array[0];
-        // 가까운 수가 여러개일 경우면 더 더 작은 수 return
-        for(int i = 1; i<array.length; i++){
-            int diff = Math.abs(array[i] - n);
-            if (diff < m) {
-                // 더 가까운 수 발견
-                m = diff;
-                answer = array[i];
-            } else if (diff == m) {
-                // 차이가 같다면 더 작은 수 선택
-                if (array[i] < answer) {
-                    answer = array[i];
-                }
+        int answer = 0;
+        Arrays.sort(array);
+        for(int i = 1 ; i < array.length ; i++){
+            if(Math.abs(n-array[0]) > Math.abs(n-array[i])){
+                array[0] = array[i];
             }
-
         }
-        
+        answer = array[0];
         return answer;
     }
 }
-
